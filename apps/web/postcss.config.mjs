9c60a7afc6a2d3e1765631618139a24ee136d6f0
sangvo235 <sangvo235@gmail.com>
2025-06-21 00:12:27 +1000

    replace-workspace-name

--- apps/web/postcss.config.mjs
+++ apps/web/postcss.config.mjs
@@ -1,1 +1,1 @@
-export { default } from "@workspace/ui/postcss.config";
+export { default } from "@sst-web-portfolio/ui/postcss.config";
--- packages/eslint-config/README.md
+++ packages/eslint-config/README.md
@@ -1,3 +1,3 @@
-# `@workspace/eslint-config`
+# `@sst-web-portfolio/eslint-config`
 
 Shared eslint configuration for the workspace.
