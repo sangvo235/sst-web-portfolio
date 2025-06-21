0aa2c4e79d543137c759451a6914d4687b3bacb5
sangvo235 <sangvo235@gmail.com>
2025-06-21 22:03:24 +1000

    general-nav-bar-set-up

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 
 import "@sst-web-portfolio/ui/globals.css"
 import { Providers } from "@/components/providers"
+import { Navbar } from "@/components/general/Navbar"
 
 const fontSans = Geist({
   subsets: ["latin"],
@@ -21,9 +22,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
+        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased max-w-7xl mx-auto px-4 sm:px-6 lg:px-8`}
       >
-        <Providers>{children}</Providers>
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
       </body>
     </html>
   )
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,9 @@
-import { Button } from "@sst-web-portfolio/ui/components/button"
+import Link from "next/link"
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World SST</h1>
-        <Button size="sm">Button</Button>
-      </div>
+    <div> 
+      <h1>Hello World from index page</h1>
     </div>
   )
 }
