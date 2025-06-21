ab6a77987d8eeb2f07291aba8b4089b248574aa3
sangvo235 <sangvo235@gmail.com>
2025-06-21 23:36:08 +1000

    add-basic-schema-and-fetch-data

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,31 @@
-import Link from "next/link"
+import { prisma } from "./utils/db";
 
-export default function Page() {
+async function getData() {
+  const data = await prisma.blogPost.findMany({
+    select: {
+      title: true,
+      content: true,
+      imageUrl: true,
+      authorImage: true,
+      authorName: true,
+      id: true,
+      createdAt: true,
+    },
+  });
+
+  return data;
+}
+
+export default async function Home() {
+  const data = await getData();
   return (
-    <div> 
-      <h1>Hello World from index page</h1>
+    <div className="py-6"> 
+      <h1 className="text-3xl font-bold tracking-tight mb-8">Latest Post</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg: grid-cols-3 gap-4"> 
+        {data.map((item) => (
+          <h1 key={item.title}>{item.title}</h1>
+        ))}
+      </div>
     </div>
   )
 }
