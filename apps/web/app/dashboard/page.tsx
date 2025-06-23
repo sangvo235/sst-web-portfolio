950af3e333fbaa13753c742788fe003e6a26261d
sangvo235 <sangvo235@gmail.com>
2025-06-23 18:02:44 +1000

    add-session

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -1,4 +1,14 @@
-export default function DashboardRoute() {
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { redirect } from "next/navigation";
+
+export default async function DashboardRoute() {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+
+    if (!user) {
+        return redirect("/api/auth/register");
+    }
+
     return (
         <div>
             <h1>hello world from the dashboard</h1>
