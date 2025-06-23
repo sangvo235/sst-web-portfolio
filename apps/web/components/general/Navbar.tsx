dc4a293ecfa987f9a2a8cea0881a41aaace4e727
sangvo235 <sangvo235@gmail.com>
2025-06-24 09:24:19 +1000

    remove-home

--- apps/web/components/general/Navbar.tsx
+++ apps/web/components/general/Navbar.tsx
@@ -12,17 +12,14 @@ export async function Navbar() {
         <nav className="w-7/8 mx-auto fixed top-0 z-50 bg-white">
             <div className="flex items-center justify-between px-4 sm:px-6 lg:px-8 py-4 border-b">
                 <div className="flex items-center gap-8">
-                <Link href="/">
-                    <div className="flex items-center gap-2">
-                        <RiHomeLine className="text-3xl text-blue-500" />
-                        <h1 className="text-3xl font-semibold text-blue-500 m-0">Sang Vo</h1>
-                    </div>
+                    <Link href="/">
+                        <div className="flex items-center gap-2">
+                            <RiHomeLine className="text-3xl text-blue-500" />
+                            <h1 className="text-3xl font-semibold text-blue-500 m-0">Sang Vo</h1>
+                        </div>
                     </Link>
 
                     <div className="flex items-center gap-6">
-                    <Link href="/" className="text-md font-medium hover:text-blue-500 transition-colors">
-                        Home
-                    </Link>
                     <Link href="/blogs" className="text-md font-medium hover:text-blue-500 transition-colors">
                         Blogs
                     </Link>
