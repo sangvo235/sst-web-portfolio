78d614830edfbca7b16755feecccd4c745baff20
sangvo235 <sangvo235@gmail.com>
2025-06-23 17:34:27 +1000

    add-auth-provider

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 import "@sst-web-portfolio/ui/globals.css"
 import { Providers } from "@/components/providers"
 import { Navbar } from "@/components/general/Navbar"
+import { AuthProvider } from '@/components/general/AuthProvider';
 
 const fontSans = Geist({
   subsets: ["latin"],
@@ -20,15 +21,17 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased max-w-7xl mx-auto px-4 sm:px-6 lg:px-8`}
-      >
-        <Providers>
-          <Navbar />
-          {children}
-        </Providers>
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased max-w-7xl mx-auto px-4 sm:px-6 lg:px-8`}
+        >
+          <Providers>
+            <Navbar />
+            {children}
+          </Providers>
+        </body>
+      </html>
+    </AuthProvider>
   )
 }
