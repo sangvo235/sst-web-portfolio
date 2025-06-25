7948093bae2ac76749ffebb0f736626de4db72c2
sangvo235 <sangvo235@gmail.com>
2025-06-26 00:43:50 +1000

    add-suspense

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,12 +6,12 @@ import { Skeleton } from '@/components/ui/skeleton';
 export default async function Home() {
   return (
     <div className="py-6"> 
-      <Suspense fallback={ <Skeleton className="w-full h-[400px]" /> }>
+      <Suspense fallback={<Skeleton className="w-full h-[400px]" />}>
         <Bio />
       </Suspense>
 
       <h1 className="text-3xl font-bold tracking-tight my-8">Latest Blog Posts</h1>
-      
+
       <Suspense
         fallback={
           <div className="flex justify-center w-full">
