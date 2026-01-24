b585ce8c415064935ec7486c0115cdaa8ad986f7
Sang Vo <sangvo235@gmail.com>
2026-01-24 18:36:51 +1100

    upgrade prisma to 7

--- apps/web/app/utils/db.ts
+++ apps/web/app/utils/db.ts
@@ -1,5 +1,6 @@
 // lib/prisma.ts
 import { PrismaClient } from "@/generated/prisma";
+import "dotenv/config";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
@@ -7,3 +8,13 @@ export const prisma =
   globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// import "dotenv/config";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@/generated/prisma";
+
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+// export const prisma = new PrismaClient({ adapter });
