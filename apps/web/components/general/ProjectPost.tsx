44033e8d3b8fc7fbdb054f910116555bc8450404
Sang Vo <sangvo235@gmail.com>
2026-01-26 03:51:35 +1100

    add project post component and skeleton

--- apps/web/components/general/ProjectPost.tsx
+++ apps/web/components/general/ProjectPost.tsx
@@ -1,25 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 import { prisma } from "@/app/utils/db";
-import { notFound } from "next/navigation";
-import { handleCommentSubmission } from "@/app/action";
-import { SubmitButton } from "@/components/general/SubmitButton";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
-import { Input } from "@/components/ui/input";
+import { notFound } from "next/navigation"
+import { handleCommentSubmission } from "@/app/action"
+import { SubmitButton } from "@/components/general/SubmitButton"
 import { buttonVariants } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from "@/components/ui/card";
-import { BiComment, BiSolidPurchaseTag } from "react-icons/bi";
+import { Card, CardContent, CardHeader, CardFooter } from "@/components/ui/card";
+import { Textarea } from "@/components/ui/textarea"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { BiComment } from "react-icons/bi";
+import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs";
 import { ArrowLeft } from "lucide-react";
 
 async function getData(id: string) {
-    const data = await prisma.blogs.findUnique({
+    const data = await prisma.projects.findUnique({
         where: { id: id },
         include: {
             comments: true,
@@ -36,13 +31,13 @@ async function getData(id: string) {
     return data;
 }
 
-export async function BlogPost ({ id, canComment }: { id: string; canComment: boolean; }) {
+export async function ProjectPost ({ id, canComment }: { id: string; canComment: boolean; }) {
     const data = await getData(id);
 
     return (
         <div className="grid grid-cols-6 gap-4">
             <div className="col-span-6 pt-4 pl-8">
-                <Link className={buttonVariants({ variant: "secondary" })} href="/blogs">
+                <Link className={buttonVariants({ variant: "secondary" })} href="/projects">
                     <ArrowLeft className="mr-1 h-4 w-4" />
                     Back to posts
                 </Link>
@@ -91,14 +86,14 @@ export async function BlogPost ({ id, canComment }: { id: string; canComment: bo
                     </div>
                 </div>
 
-                <div>
-                    <Badge
-                        variant="secondary"
-                        className="bg-blue-500 text-white dark:bg-blue-600"
-                    >
-                        <BiSolidPurchaseTag />
-                        {data.topic}
-                    </Badge>
+                <div className="flex items-center gap-4">
+                    <Link href={data.githubUrl} >
+                        <BsGithub className="size-8 hover:text-blue-500 hover:-translate-y-1 transition-transform cursor-pointer" />
+                    </Link>
+
+                    <Link href={data.demoUrl} >
+                        <BsArrowUpRightSquare className="size-8 hover:text-blue-500 hover:-translate-y-1 transition-transform cursor-pointer" />
+                    </Link>
                 </div>
             </div>
 
@@ -111,15 +106,34 @@ export async function BlogPost ({ id, canComment }: { id: string; canComment: bo
                 />
             </div>
 
-            <div className="col-start-2 col-span-4 text-md text-gray-600 py-4">
+            <div className="col-start-2 col-span-4 py-2 flex flex-col items-center space-y-2">
+                <div className="text-xl font-semibold">Tech Stack</div>
+
+                {data.techIconUrls && data.techIconUrls.length > 0 && (
+                    <div className="flex flex-wrap justify-center gap-2">
+                        {data.techIconUrls.map((iconUrl, idx) => (
+                            <div key={idx} className="w-8 h-8 relative">
+                                <Image
+                                src={iconUrl}
+                                alt={`tech icon ${idx + 1} for Project`}
+                                fill
+                                className="object-contain"
+                                />
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+  
+            <div className="col-start-2 col-span-4 text-md text-gray-600 py-2">
                 {data.content}
             </div>
-            
+          
             <span id="comments" />
 
-            <div className="col-start-2 col-span-4 text-xl font-semibold">
+            <p className="col-start-2 col-span-4 text-xl font-semibold">
                 Comments
-            </div>
+            </p>
 
             {data.comments.length === 0 ? (
                 <div className="col-start-2 col-span-4 text-sm text-gray-500 italic">
@@ -132,16 +146,16 @@ export async function BlogPost ({ id, canComment }: { id: string; canComment: bo
                             <div className="flex items-center justify-between">
                                 <div className="flex items-center gap-2">
                                     <div className="relative size-8 overflow-hidden rounded-full">
-                                        <Image
-                                            src={comment.authorImage}
-                                            alt={`${comment.authorFirstName} ${comment.authorLastName}`}
-                                            fill
-                                            className="object-cover"
-                                        />
+                                    <Image
+                                        src={comment.authorImage}
+                                        alt={`${comment.authorFirstName} ${comment.authorLastName}`}
+                                        fill
+                                        className="object-cover"
+                                    />
                                     </div>
-                                    <div className="text-sm font-medium text-gray-700">
+                                    <p className="text-sm font-medium text-gray-700">
                                         {comment.authorFirstName} {comment.authorLastName}
-                                    </div>
+                                    </p>
                                 </div>
 
                                 <div className="text-sm text-gray-500">
@@ -155,7 +169,7 @@ export async function BlogPost ({ id, canComment }: { id: string; canComment: bo
                                 </div>
                             </div>
                         </CardHeader>
-
+                        
                         <CardContent>
                             <div className="text-sm text-gray-800 pl-8">
                                 {comment.content}
@@ -165,7 +179,7 @@ export async function BlogPost ({ id, canComment }: { id: string; canComment: bo
                     </Card>
                 ))
             )}
-            
+        
             {canComment && (
                 <div className="col-start-2 col-span-4 px-6">
                     <Label className="py-2">Add your comment</Label>
@@ -185,3 +199,6 @@ export async function BlogPost ({ id, canComment }: { id: string; canComment: bo
         </div>
     );
 }
+
+// TODO: Mobile Views
+// TODO: Image Link to Project
